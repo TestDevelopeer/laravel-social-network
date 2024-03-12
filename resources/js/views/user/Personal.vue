7985afbd22e2320608a51f9aa7f46468192e509c
TestDevelopeer <tdevelopeer@gmail.com>
2024-03-12 15:56:23 +0300

    Форма отправки поста

--- resources/js/views/user/Personal.vue
+++ resources/js/views/user/Personal.vue
@@ -1,6 +1,17 @@
 <template>
     <div class="w-96 mx-auto">
-        Personal
+        <div>
+            <div>
+                <input class="w-96 mb-3 rounded-3xl border p-2 border-slate-300" type="text" placeholder="title">
+            </div>
+            <div>
+                <textarea class="w-96 mb-3 rounded-3xl border p-2 border-slate-300" placeholder="content"></textarea>
+            </div>
+            <div>
+                <a href="#"
+                   class="block p-2 w-32 rounded-3xl text-center box-border bg-green-600 text-white hover:bg-white hover:border hover:border-green-600 hover:text-green-600">Publish</a>
+            </div>
+        </div>
     </div>
 </template>
 
